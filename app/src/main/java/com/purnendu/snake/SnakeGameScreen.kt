7cbf3df96e5364670760c0626aa9fa15d819cc65
PurnenduSamanta <joysamanta123@gmail.com>
2024-01-10 22:49:49 +0530

    2nd_10_01_2024

--- app/src/main/java/com/purnendu/snake/SnakeGameScreen.kt
+++ app/src/main/java/com/purnendu/snake/SnakeGameScreen.kt
@@ -1,11 +1,16 @@
 package com.purnendu.snake
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,15 +18,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.purnendu.snake.ui.theme.Custard
 import com.purnendu.snake.ui.theme.RoyalBlue
 
 @Composable
-fun SnakeGameScreen() {
+fun SnakeGameScreen(
+    state: SnakeGameState
+) {
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    val foodImageBitmap = ImageBitmap.imageResource(id = R.drawable.img_apple)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
 
         Card(
             modifier = Modifier
@@ -31,7 +47,7 @@ fun SnakeGameScreen() {
 
             Text(
                 modifier = Modifier.padding(16.dp),
-                text = "Score: 5",
+                text = "Score: ${state.snake.size - 1}",
                 style = MaterialTheme.typography.headlineMedium
             )
         }
@@ -46,11 +62,38 @@ fun SnakeGameScreen() {
                 cellSize = size.width / 20,
                 cellColor = Custard,
                 borderCellColor = RoyalBlue,
-                gridWidth = 20,
-                gridHeight = 30
+                gridWidth = state.xAxisGridSize,
+                gridHeight = state.yAxisGridSize
+            )
+
+            drawFood(
+                foodImage = foodImageBitmap,
+                cellSize = (size.width / 20).toInt(),
+                coordinate = state.food
             )
 
 
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { }) {
+                Text(text = "Replay")
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { }) {
+                Text(text = "Start")
+            }
         }
 
     }
@@ -69,13 +112,30 @@ private fun DrawScope.drawGameBoard(
 
     for (i in 0 until gridWidth) {
         for (j in 0 until gridHeight) {
-            val isBorderCell= i==0 || j==0 || i== gridWidth -1 || j==gridHeight-1
+            val isBorderCell = i == 0 || j == 0 || i == gridWidth - 1 || j == gridHeight - 1
             drawRect(
-                color = if(isBorderCell) borderCellColor else if((i+j)%2==0) cellColor else cellColor.copy(alpha = 0.5f),
-                topLeft = Offset(x=i*cellSize,y=j*cellSize),
+                color = if (isBorderCell) borderCellColor else if ((i + j) % 2 == 0) cellColor else cellColor.copy(
+                    alpha = 0.5f
+                ),
+                topLeft = Offset(x = i * cellSize, y = j * cellSize),
                 size = Size(width = cellSize, height = cellSize)
             )
         }
     }
 
+}
+
+private fun DrawScope.drawFood(
+    foodImage: ImageBitmap,
+    cellSize: Int,
+    coordinate: Coordinate
+) {
+    drawImage(
+        image = foodImage,
+        dstOffset = IntOffset(
+            x = (coordinate.x * cellSize),
+            y = (coordinate.y * cellSize)
+        ),
+        dstSize = IntSize(cellSize, cellSize)
+    )
 }
---
+++ app/src/main/java/com/purnendu/snake/SnakeGameState.kt
@@ -0,0 +1,41 @@
+package com.purnendu.snake
+
+import kotlin.random.Random
+
+data class SnakeGameState(
+    val xAxisGridSize: Int = 20,
+    val yAxisGridSize: Int = 30,
+    val direction: Direction =Direction.RIGHT,
+    val snake:List<Coordinate> = listOf(Coordinate(x=5,y=5)),
+    val food:Coordinate = generateRandomFoodCoordinates(),
+    val isGameOver: Boolean = false,
+    val gameState: GameState = GameState.IDLE
+)
+{
+    companion object{
+        fun generateRandomFoodCoordinates():Coordinate
+        {
+            return Coordinate(
+                x= Random.nextInt(from = 1, until = 19),
+                y= Random.nextInt(from = 1, until =29)
+            )
+        }
+    }
+}
+
+enum class GameState
+{
+    IDLE,
+    STARTED,
+    PAUSED
+}
+
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+data class Coordinate(val x: Int, val y: Int)
